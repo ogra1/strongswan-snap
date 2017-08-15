33c1e3f87657614c3aa7766ba3f787a3153aa46b
Oliver Grawert <ogra@ubuntu.com>
2017-08-15 13:32:12 +0200

    initial commit

---
+++ wrappers/pki-wrapper.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+set -e
+
+export LD_LIBRARY_PATH="$SNAP/usr/lib/ipsec/plugins:$SNAP/usr/lib/ipsec:$LD_LIBRARY_PATH"
+
+if [ "$(id -u)" != "0" ]; then
+    echo "$(basename $0) requires sudo"
+    exit 1
+fi
+
+[ -d /var/snap/strongswan/current/etc ] || cp -a $SNAP/var/snap/strongswan/current/etc $SNAP_DATA
+
+$SNAP/usr/bin/pki "$@"
